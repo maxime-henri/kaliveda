2c5ad4146158d2e9ffd152e10b5e073641f7e499
John Frankland <john.frankland@ganil.fr>
2019-07-24 17:37:29 +0200

    further refinements to KVSmoothIPDist

--- KVMultiDet/analysis/KVSmoothIPDist.cpp
+++ KVMultiDet/analysis/KVSmoothIPDist.cpp
@@ -19,19 +19,87 @@ ClassImp(KVSmoothIPDist)
 
 Double_t smooth_pb(Double_t* x, Double_t* par)
 {
+   // Smooth impact parameter distribution (arbitrary normalisation par[0])
+   // x = b
+   // par[1] = b0
+   // par[2] = delta_b
    // f(b) = [0] * x * [ 1 + exp((x - [1])/[2]) ]**-1
 
-   if (par[0]*par[2] == 0) return 0;
+   if (par[0] == 0) return 0;
+
+   if (par[2] == 0) {
+      //sharp cutoff
+      if (x[0] <= par[1]) return par[0] * x[0];
+      return 0;
+   }
 
    return par[0] * x[0] / (1. + TMath::Exp((x[0] - par[1]) / par[2]));
 }
 
+Double_t sigma_inel(Double_t* x, Double_t* p)
+{
+   // reaction cross-section [mb] as function of b0 and deltab
+   // x=b0
+   // p[0]=deltab
+
+   if (p[0] == 0) {
+      // sharp cut-off
+      return TMath::Pi() * TMath::Power(x[0], 2) * 10.;
+   }
+   return -10 * TMath::TwoPi() * TMath::Power(p[0], 2) *
+          TMath::DiLog(-TMath::Exp(x[0] / p[0]));
+}
+
+double ana_centrality(double* x, double* par)
+{
+   // Analytic expression for centrality
+   //x=b
+   //par(0)=b_0
+   //par(1)=deltab
+
+   double b = x[0];
+   if (b <= 0) return 0;
+   double b0 = par[0];
+   if (b0 <= 0) return 0;
+   double db = par[1];
+   if (db == 0) {
+      // sharp cut-off
+      return TMath::Power(b / b0, 2);
+   }
+   const double pi_sqr_over_6 = TMath::Power(TMath::Pi(), 2) / 6.;
+   double exp_b_minus_b0_over_db = TMath::Exp((b - b0) / db);
+   double db_sqr = db * db;
+   double sigma = sigma_inel(&b0, &db) / 10.; // in fm**2
+   double cb = -TMath::DiLog(-TMath::Exp(b0 / db)) - pi_sqr_over_6
+               + (b * b - b0 * b0) / 2. / db_sqr
+               - (b / db) * TMath::Log(1 + exp_b_minus_b0_over_db)
+               - TMath::DiLog(-exp_b_minus_b0_over_db);
+   cb *= TMath::TwoPi() * db_sqr / sigma;
+   return cb;
+}
+
+KVSmoothIPDist::KVSmoothIPDist()
+   : fHisto(nullptr),
+     fIPdist("smooth_pb", smooth_pb, 0., 20., 3),
+     fSigmaR("sigmaR", sigma_inel, 0., 20., 1),
+     fCentrality("centrality", ana_centrality, 0., 20., 2)
+{
+   fIPdist.SetParNames("Norm", "b_{0}", "#Delta b");
+   fSigmaR.SetParName(0, "#Delta b");
+   fCentrality.SetParNames("b_{0}", "#Delta b");
+}
+
 KVSmoothIPDist::KVSmoothIPDist(TH1* h)
-   : TF1(h->GetName(), smooth_pb, 0., 50., 3), fHisto(h)
+   : fHisto(h),
+     fIPdist(Form("%s_smooth_pb", h->GetName()), smooth_pb, 0., 50., 3),
+     fSigmaR(Form("%s_sigmaR", h->GetName()), sigma_inel, 0., 50., 1),
+     fCentrality(Form("%s_centrality", h->GetName()), ana_centrality, 0., 20., 2)
 {
    // Fit the given impact parameter distribution
 
-   SetParNames("Norm", "b_{0}", "#Delta b");
+   fIPdist.SetParNames("Norm", "b_{0}", "#Delta b");
+   fSigmaR.SetParName(0, "#Delta b");
+   fCentrality.SetParNames("b_{0}", "#Delta b");
    FitIPDist(h);
 }
 
@@ -43,20 +111,22 @@ void KVSmoothIPDist::FitIPDist(TH1* h)
    Double_t maxb = h->GetXaxis()->GetBinUpEdge(h->GetXaxis()->GetNbins());
    Double_t maxy = h->GetMaximum();
    Double_t bmax = h->GetBinCenter(h->GetMaximumBin());
-   SetParLimits(0, 1., maxy);
-   SetParLimits(1, minb, maxb);
-   SetParLimits(2, 1.e-6, maxb - minb);
-   SetParameters(maxy / 10., bmax, 0.4);
-   h->Fit(this, "I");
+   fIPdist.SetParLimits(0, 1., maxy);
+   fIPdist.SetParLimits(1, minb, maxb);
+   fIPdist.SetParLimits(2, 1.e-6, maxb - minb);
+   fIPdist.SetParameters(maxy / 10., bmax, 0.4);
+   h->Fit(&fIPdist, "I");
+   fSigmaR.SetParameter(0, GetDeltaB());
+   fCentrality.SetParameter(0, GetB0());
+   fCentrality.SetParameter(1, GetDeltaB());
 }
 
 Double_t KVSmoothIPDist::GetCrossSection() const
 {
-   // Returns calculated total reaction cross-section in [mb] using parameters of
-   // function fit to distribution by FitIPDist
+   // Returns calculated total reaction cross-section in [mb] using
+   // current values of b0 and delta_b parameters
 
-   return -10.*TMath::TwoPi() * TMath::Power(GetDeltaB(), 2) *
-          TMath::DiLog(-TMath::Exp(GetB0() / GetDeltaB()));
+   return fSigmaR.Eval(GetB0());
 }
 
 Double_t KVSmoothIPDist::GetCrossSectionPerEvent() const
@@ -66,3 +136,20 @@ Double_t KVSmoothIPDist::GetCrossSectionPerEvent() const
    return fHisto->GetEntries() ? GetCrossSection() / fHisto->GetEntries() : 0.;
 }
 
+void KVSmoothIPDist::SetDeltaB_WithConstantCrossSection(Double_t deltab, Double_t sigmaR)
+{
+   // Change deltab and modify b0 in order to have total cross section sigmaR
+   // If sigmaR=0 (default), keep current total cross section
+
+   Double_t sigma = sigmaR > 0 ? sigmaR : const_cast<KVSmoothIPDist*>(this)->GetCrossSection();
+   SetDeltaB(deltab);
+   Double_t b0 = fSigmaR.GetX(sigma);
+   SetB0(b0);
+}
+
+Double_t KVSmoothIPDist::Calculate_b(Double_t centrality) const
+{
+   // calculate b for given value of centrality using current parameters
+
+   return fCentrality.GetX(centrality);
+}
--- KVMultiDet/analysis/KVSmoothIPDist.h
+++ KVMultiDet/analysis/KVSmoothIPDist.h
@@ -4,17 +4,17 @@
 #ifndef __KVSMOOTHIPDIST_H
 #define __KVSMOOTHIPDIST_H
 
+#include "KVBase.h"
 #include "TF1.h"
 
-class KVSmoothIPDist : public TF1 {
+class KVSmoothIPDist : public KVBase {
    TH1*  fHisto;//! last fitted histogram
+   TF1 fIPdist;// impact parameter distribution
+   TF1 fSigmaR;// total reaction cross section
+   TF1 fCentrality;//
 
 public:
-   KVSmoothIPDist()
-      : TF1(), fHisto(nullptr)
-   {
-      // Default constructor
-   }
+   KVSmoothIPDist();
    KVSmoothIPDist(TH1*);
    virtual ~KVSmoothIPDist() {}
 
@@ -23,13 +23,38 @@ public:
    Double_t GetCrossSection() const;
    Double_t GetB0() const
    {
-      return GetParameter(1);
+      return fIPdist.GetParameter(1);
    }
    Double_t GetDeltaB() const
    {
-      return GetParameter(2);
+      return fIPdist.GetParameter(2);
    }
    Double_t GetCrossSectionPerEvent() const;
+   void SetB0(Double_t x)
+   {
+      fIPdist.SetParameter(1, x);
+      fCentrality.SetParameter(0, x);
+   }
+   void SetDeltaB(Double_t x)
+   {
+      fIPdist.SetParameter(2, x);
+      fSigmaR.SetParameter(0, x);
+      fCentrality.SetParameter(1, x);
+   }
+   void SetDeltaB_WithConstantCrossSection(Double_t deltab, Double_t sigmaR = 0);
+   const TF1& GetIPDist() const
+   {
+      return fIPdist;
+   }
+   const TF1& GetSigmaR() const
+   {
+      return fSigmaR;
+   }
+   const TF1& GetCentrality() const
+   {
+      return fCentrality;
+   }
+   Double_t Calculate_b(Double_t centrality) const;
 
    ClassDef(KVSmoothIPDist, 1) //Triangular IP distribution with smooth fall-off
 };
