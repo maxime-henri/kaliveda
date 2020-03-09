maxime-henri/kaliveda

commit 870cab4325550019ce99e5b9e471a975f155177a

fix potential memory leak in KV2Body & update interface
 * using the constructor KV2Body(KVNucleus*, KVNucleus*) like this:

      KV2Body q(new KVNucleus("129Xe",50), new KVNucleus("natSn"), new KVNucleus(...))

  lead to memory leak (projectile, target and outgoing were not deleted
  in dtor when set up in this way).

KVMultiDet/db/KV2Body.h
@@ -21,14 +21,14 @@ $Id: KV2Body.h,v 1.5 2009/02/02 13:52:29 ebonnet Exp $
 #define KV2BODY_H
 
 #include "TObject.h"
-#include "TObjArray.h"
+#include <vector>
 #include "TVector3.h"
 #include "KVNucleus.h"
 #include "TF1.h"
 
 class KV2Body: public TObject {
 
-   TObjArray fNuclei;           //nuclei involved in calculation
+   std::vector<KVNucleus> fNuclei; //nuclei involved in calculation
    Double_t fEDiss;             //dissipated energy, 0 means elastic scattering
 
    TVector3 VCM;                //velocity of centre of mass
@@ -43,14 +43,10 @@ class KV2Body: public TObject {
    Double_t TETAMAX[5];          //defined only for nuclei 3 et 4
    Double_t TETAMIN[5];          //defined only for nuclei 3 et 4
 
-   Bool_t fDeleteTarget;
-   Bool_t fDeleteProj;
-   Bool_t fDeleteN4;
-
-   TF1* fKoxReactionXSec;   // function Kox reaction cross-section [barns] vs. E/A projectile
-   TF1* fEqbmChargeState;   // function equilibrium charge state of projectile vs. E/A projectile (Leon et al)
-   TF1* fEqbmChargeStateShSol;   // function equilibrium charge state of projectile vs. E/A projectile (Shiwietz et al solid)
-   TF1* fEqbmChargeStateShGas;   // function equilibrium charge state of projectile vs. E/A projectile (Shiwietz et al gas)
+   mutable TF1* fKoxReactionXSec;   // function Kox reaction cross-section [barns] vs. E/A projectile
+   mutable TF1* fEqbmChargeState;   // function equilibrium charge state of projectile vs. E/A projectile (Leon et al)
+   mutable TF1* fEqbmChargeStateShSol;   // function equilibrium charge state of projectile vs. E/A projectile (Shiwietz et al solid)
+   mutable TF1* fEqbmChargeStateShGas;   // function equilibrium charge state of projectile vs. E/A projectile (Shiwietz et al gas)
 
    void Set4thNucleus();
    Double_t ThetaLabVsThetaCM(Double_t*, Double_t*);
@@ -61,12 +57,12 @@ class KV2Body: public TObject {
    Double_t XSecRuthCM(Double_t*, Double_t*);
    Double_t XSecRuthCMVsThetaCM(Double_t*, Double_t*);
 
-   TF1* fThetaLabVsThetaCM[5];
-   TF1* fELabVsThetaCM[5];
-   TF1* fELabVsThetaLab[5];
+   mutable TF1* fThetaLabVsThetaCM[5];
+   mutable TF1* fELabVsThetaCM[5];
+   mutable TF1* fELabVsThetaLab[5];
 
-   TF1* fXSecRuthLabIntegral[5];
-   TF1* fXSecRuthLab[5];
+   mutable TF1* fXSecRuthLabIntegral[5];
+   mutable TF1* fXSecRuthLab[5];
 
    Bool_t fSetOutgoing;// = kTRUE if SetOutgoing is called before CalculateKinematics
 
@@ -78,19 +74,21 @@ public:
    void init();
    KV2Body();
    KV2Body(const Char_t* systemname);
-   KV2Body(KVNucleus* proj, KVNucleus* cib = 0, KVNucleus* proj_out =
-              0, Double_t Ediss = 0.0);
+   KV2Body(const KVNucleus& compound, double Exx = 0.0);
+   KV2Body(const KVNucleus& proj, const KVNucleus& targ, double Ediss = 0.0);
+   KV2Body(const KVNucleus& proj, const KVNucleus& targ, const KVNucleus& proj_out, double Ediss = 0.0);
+   KV2Body(KVNucleus* proj, KVNucleus* cib = nullptr, KVNucleus* proj_out = nullptr, Double_t Ediss = 0.0);
    virtual ~ KV2Body();
 
    void CalculateKinematics();
 
    static Double_t GetVelocity(Double_t mass, Double_t E);
 
-   void SetProjectile(KVNucleus*);
+   void SetProjectile(const KVNucleus&);
    void SetProjectile(Int_t z, Int_t a = 0);
-   void SetTarget(KVNucleus*);
+   void SetTarget(const KVNucleus&);
    void SetTarget(Int_t z, Int_t a = 0);
-   void SetOutgoing(KVNucleus* proj_out);
+   void SetOutgoing(const KVNucleus& proj_out);
 
    void SetExcitEnergy(Double_t ex)
    {
@@ -103,7 +101,7 @@ public:
    void SetEDiss(Double_t ex)
    {
       SetExcitEnergy(ex);
-   };
+   }
    Double_t GetEDiss() const
    {
       return GetExcitEnergy();
@@ -135,9 +133,9 @@ public:
       return gamma;
    };
 
-   TF1* GetThetaLabVsThetaCMFunc(Int_t OfNucleus);
-   TF1* GetELabVsThetaCMFunc(Int_t OfNucleus);
-   TF1* GetELabVsThetaLabFunc(Int_t OfNucleus);
+   TF1* GetThetaLabVsThetaCMFunc(Int_t OfNucleus) const;
+   TF1* GetELabVsThetaCMFunc(Int_t OfNucleus) const;
+   TF1* GetELabVsThetaLabFunc(Int_t OfNucleus) const;
 
    Double_t GetThetaLab(Double_t ThetaCM, Int_t OfNucleus) const
    {
@@ -169,25 +167,25 @@ public:
    //Double_t GetIntegratedXSecRuthLab(KVTelescope* tel, Int_t OfNucleus = 3);
    //Double_t GetIntegratedXSecRuthLab(KVDetector* det, Int_t OfNucleus = 3);
 
-   TF1* GetXSecRuthLabFunc(Int_t OfNucleus = 3, Double_t theta_min = 1., Double_t theta_max = 179.);
-   TF1* GetXSecRuthLabIntegralFunc(Int_t OfNucleus = 3, Double_t theta_min = 1., Double_t theta_max = 179.);
+   TF1* GetXSecRuthLabFunc(Int_t OfNucleus = 3, Double_t theta_min = 1., Double_t theta_max = 179.) const;
+   TF1* GetXSecRuthLabIntegralFunc(Int_t OfNucleus = 3, Double_t theta_min = 1., Double_t theta_max = 179.) const;
 
    void Print(Option_t* opt = "") const;
 
    Double_t BassIntBarrier();
    Double_t KoxReactionXSec(Double_t*, Double_t*);
-   TF1* GetKoxReactionXSecFunc();
+   TF1* GetKoxReactionXSecFunc() const;
 
    Double_t GetSphereDureReactionXSec(Double_t r0 = 1.05);
    Double_t GetBmaxFromReactionXSec(Double_t ReacXsec);
    Double_t GetIntegratedXsec(Double_t b1, Double_t b2);
 
    Double_t EqbmChargeState(Double_t* t, Double_t*);
-   TF1* GetEqbmChargeStateFunc();
+   TF1* GetEqbmChargeStateFunc() const;
    Double_t eqbm_charge_state_shiwietz_solid(Double_t* t, Double_t*);
-   TF1* GetShiwietzEqbmChargeStateFuncForSolidTargets();
+   TF1* GetShiwietzEqbmChargeStateFuncForSolidTargets() const;
    Double_t eqbm_charge_state_shiwietz_gas(Double_t* t, Double_t*);
-   TF1* GetShiwietzEqbmChargeStateFuncForGasTargets();
+   TF1* GetShiwietzEqbmChargeStateFuncForGasTargets() const;
 
    Double_t GetIntegralPrecision()
    {

KVMultiDet/simulation/KVElasticCountRates.cpp
@@ -138,7 +138,6 @@ KVElasticCountRates::KVElasticCountRates(Double_t theta_min, Double_t theta_max,
    fEnergy = 0;
    fKinematics = 0;
    fTarget = 0;
-   fProj = fTarg = 0;
    fIntLayer =  0;
    fMultiLayer = kFALSE;
    fVolume = (theta_max - theta_min) * (phi_max - phi_min) * TMath::DegToRad() * TMath::DegToRad();
@@ -166,10 +165,6 @@ KVElasticCountRates::~KVElasticCountRates()
       delete fTheta;
    if (fKinematics)
       delete fKinematics;
-   if (fProj)
-      delete fProj;
-   if (fTarg)
-      delete fTarg;
 }
 
 //__________________________________________________________________//
@@ -190,8 +185,8 @@ void KVElasticCountRates::SetRun(Int_t run)
 void KVElasticCountRates::SetProjectile(const Char_t* nuc, Double_t e_sur_a)
 {
    // Set projectile and beam energy [MeV/nucleon]
-   if (!fProj) fProj = new KVNucleus(nuc, e_sur_a);
-   fEnergy = fProj->GetE();
+   fProj = KVNucleus(nuc, e_sur_a);
+   fEnergy = fProj.GetE();
 }
 
 //__________________________________________________________________//
@@ -236,7 +231,7 @@ void KVElasticCountRates::CalculateScattering(Int_t N)
    fHistos.Delete();
    fRates.clear();
 
-   if (!fProj) {
+   if (!fProj.IsDefined()) {
       cout <<
            "<KVElasticCountRates::CalculateScattering> : Set projectile properties first"
            << endl;
@@ -275,8 +270,8 @@ void KVElasticCountRates::CalculateScattering(Int_t N)
    //set up kinematics
    if (!fKinematics)
       fKinematics = new KV2Body;
-   fProj->SetEnergy(fEnergy);
-   fProj->SetTheta(0);
+   fProj.SetEnergy(fEnergy);
+   fProj.SetTheta(0);
 
    /* -------------------------------------------------------------------------------------------------------------------------- */
 
@@ -285,7 +280,7 @@ void KVElasticCountRates::CalculateScattering(Int_t N)
       fTarget->SetInteractionLayer(fIntLayer, fBeamDirection);
    }
    else {
-      fTarget->GetInteractionPoint(fProj);
+      fTarget->GetInteractionPoint(&fProj);
    }
 
    /* -------------------------------------------------------------------------------------------------------------------------- */
@@ -293,10 +288,9 @@ void KVElasticCountRates::CalculateScattering(Int_t N)
    //get target nucleus properties from scattering layer
    TVector3 IP = fTarget->GetInteractionPoint();
    KVMaterial* targ_mat = fTarget->GetLayer(IP);
-   if (!fTarg) fTarg = new KVNucleus;
-   fTarg->SetZ((Int_t) targ_mat->GetZ());
-   fTarg->SetA((Int_t) targ_mat->GetMass());
-   fKinematics->SetTarget(fTarg);
+   KVNucleus t;
+   t.SetZandA((Int_t) targ_mat->GetZ(), (Int_t) targ_mat->GetMass());
+   fKinematics->SetTarget(t);
 
    /* -------------------------------------------------------------------------------------------------------------------------- */
 
@@ -309,7 +303,7 @@ void KVElasticCountRates::CalculateScattering(Int_t N)
    for (int i = 0; i < N; i++) {
       //calculate slowing of incoming projectile
       fTarget->SetIncoming();
-      fTarget->DetectParticle(fProj);
+      fTarget->DetectParticle(&fProj);
       fKinematics->SetProjectile(fProj);
       fKinematics->SetOutgoing(fProj);
       fKinematics->CalculateKinematics();
@@ -322,35 +316,34 @@ void KVElasticCountRates::CalculateScattering(Int_t N)
       //We only use the highest energy corresponding to the most forward CM angle.
       Double_t e1, e2;
       fKinematics->GetELab(3, theta, 3, e1, e2);
-      fProj->SetEnergy(TMath::Max(e1, e2));
-      fProj->SetTheta(theta);
-      fProj->SetPhi(phi);
+      fProj.SetEnergy(TMath::Max(e1, e2));
+      fProj.SetTheta(theta);
+      fProj.SetPhi(phi);
       fTheta->Fill(theta, xsec);
       //slowing of outgoing projectile in target
       fTarget->SetOutgoing();
-      fTarget->DetectParticle(fProj);
+      fTarget->DetectParticle(&fProj);
       //now detect particle in array
-      KVNameValueList* detectors = gMultiDetArray->DetectParticle(fProj);
+      KVNameValueList* detectors = gMultiDetArray->DetectParticle(&fProj);
       //fill histograms
       fDepth->Fill(IP.z());
       FillHistograms(detectors);
-      fProj->GetParameters()->Clear();
-      fProj->SetEnergy(fEnergy);
-      fProj->SetTheta(0);
+      fProj.GetParameters()->Clear();
+      fProj.SetEnergy(fEnergy);
+      fProj.SetTheta(0);
       //set random interaction point for scattering
       if (fIntLayer) {
          fTarget->SetInteractionLayer(fIntLayer, fBeamDirection);
       }
       else {
-         fTarget->GetInteractionPoint(fProj);
+         fTarget->GetInteractionPoint(&fProj);
          //if target is multilayer and the interaction layer is not fixed,
          //the layer & hence the target nucleus may change
          if (fMultiLayer) {
             targ_mat = fTarget->GetLayer(fTarget->GetInteractionPoint());
-            fTarg->SetZ((Int_t) targ_mat->GetZ());
-            fTarg->SetA((Int_t) targ_mat->GetMass());
-            fKinematics->SetTarget(fTarg);
-            fKinematics->SetOutgoing(fProj);
+            KVNucleus t;
+            t.SetZandA((Int_t) targ_mat->GetZ(), (Int_t) targ_mat->GetMass());
+            fKinematics->SetTarget(t);
          }
       }
       IP = fTarget->GetInteractionPoint();

KVMultiDet/simulation/KVElasticCountRates.h
@@ -33,8 +33,7 @@ class KVElasticCountRates {
    Int_t  fBinE;                //Number of bins of the Energy histogram
 
    Double_t fEnergy;            //energy of projectile
-   KVNucleus* fProj;            //scattered nucleus
-   KVNucleus* fTarg;            //target nucleus
+   KVNucleus fProj;            //scattered nucleus
 
    KVPosition fAngularRange;    //angular range in which to scatter
 

KVMultiDet/simulation/KVElasticScatter.cpp
@@ -15,7 +15,6 @@ $Date: 2007/04/04 10:39:17 $
 #include "KVGroup.h"
 #include "KVTarget.h"
 #include "KV2Body.h"
-#include "KVNucleus.h"
 #include "TObjArray.h"
 
 using namespace std;
@@ -95,7 +94,6 @@ KVElasticScatter::KVElasticScatter(): fBeamDirection(0, 0, 1)
    fKinematics = 0;
    fTelescope = 0;
    fTarget = 0;
-   fProj = fTarg = 0;
    fIntLayer = fNDets = 0;
    fDetector = 0;
    fMultiLayer = kFALSE;
@@ -123,10 +121,6 @@ KVElasticScatter::~KVElasticScatter()
       delete fTheta;
    if (fKinematics)
       delete fKinematics;
-   if (fProj)
-      delete fProj;
-   if (fTarg)
-      delete fTarg;
    if (fHistos)
       delete fHistos;
    if (fDetInd)
@@ -151,10 +145,8 @@ void KVElasticScatter::SetRun(Int_t run)
 void KVElasticScatter::SetProjectile(Int_t Z, Int_t A)
 {
    //Set projectile Z and A
-   if (!fProj)
-      fProj = new KVNucleus;
-   fProj->SetZ(Z);
-   fProj->SetA(A);
+
+   fProj.SetZandA(Z, A);
 }
 
 //__________________________________________________________________//
@@ -162,8 +154,8 @@ void KVElasticScatter::SetProjectile(Int_t Z, Int_t A)
 void KVElasticScatter::SetEnergy(Double_t e)
 {
    //Set energy of projectile in MeV
-   if (fProj)
-      fProj->SetEnergy(e);
+
+   fProj.SetEnergy(e);
    fEnergy = e;
 }
 
@@ -239,7 +231,7 @@ void KVElasticScatter::CalculateScattering(Int_t N)
    //Perform scattering 'N' times for current values
    //of particle Z, A and energy, target excited state, and detector.
 
-   if (!fProj) {
+   if (!fProj.IsDefined()) {
       cout <<
            "<KVElasticScatter::CalculateScattering> : Set projectile properties first"
            << endl;
@@ -303,8 +295,8 @@ void KVElasticScatter::CalculateScattering(Int_t N)
    //set up kinematics
    if (!fKinematics)
       fKinematics = new KV2Body;
-   fProj->SetEnergy(fEnergy);
-   fProj->SetTheta(0);
+   fProj.SetEnergy(fEnergy);
+   fProj.SetTheta(0);
 
    /* -------------------------------------------------------------------------------------------------------------------------- */
 
@@ -313,7 +305,7 @@ void KVElasticScatter::CalculateScattering(Int_t N)
       fTarget->SetInteractionLayer(fIntLayer, fBeamDirection);
    }
    else {
-      fTarget->GetInteractionPoint(fProj);
+      fTarget->GetInteractionPoint(&fProj);
    }
 
    /* -------------------------------------------------------------------------------------------------------------------------- */
@@ -321,13 +313,10 @@ void KVElasticScatter::CalculateScattering(Int_t N)
    //get target nucleus properties from scattering layer
    TVector3 IP = fTarget->GetInteractionPoint();
    KVMaterial* targ_mat = fTarget->GetLayer(IP);
-   if (!fTarg)
-      fTarg = new KVNucleus;
-   fTarg->SetZ((Int_t) targ_mat->GetZ());
-   fTarg->SetA((Int_t) targ_mat->GetMass());
-   fKinematics->SetProjectile(fProj);
-   fKinematics->SetTarget(fTarg);
-   fKinematics->SetOutgoing(fProj);
+   KVNucleus t;
+   t.SetZ((Int_t) targ_mat->GetZ());
+   t.SetA((Int_t) targ_mat->GetMass());
+   fKinematics->SetTarget(t);
 
    /* -------------------------------------------------------------------------------------------------------------------------- */
 
@@ -341,36 +330,32 @@ void KVElasticScatter::CalculateScattering(Int_t N)
    for (int i = 0; i < N; i++) {
       //calculate slowing of incoming projectile
       fTarget->SetIncoming();
-      fTarget->DetectParticle(fProj);
+      fTarget->DetectParticle(&fProj);
+      fKinematics->SetProjectile(fProj);
+      fKinematics->SetOutgoing(fProj);
       fKinematics->CalculateKinematics();
       //set random direction of outgoing projectile
 
       double th, ph;
       th = ph = 0.;
       fDetector->GetRandomAngles(th, ph);
-      fProj->SetEnergy(fProj->GetEnergy());
-      fProj->SetTheta(th);
-      fProj->SetPhi(ph);
 
-//      fProj->SetRandomMomentum(fProj->GetEnergy(),
-//                               fTelescope->GetThetaMin(),
-//                               fTelescope->GetThetaMax(),
-//                               fTelescope->GetPhiMin(),
-//                               fTelescope->GetPhiMax(), "random");
       //set energy of scattered nucleus
       //WARNING: for inverse kinematics reactions, their are two energies for
       //each angle below the maximum scattering angle.
       //We only use the highest energy corresponding to the most forward CM angle.
       Double_t e1, e2;
-      fKinematics->GetELab(3, fProj->GetTheta(), 3, e1, e2);
-      fProj->SetEnergy(TMath::Max(e1, e2));
-      xsec = TMath::Abs(fKinematics->GetXSecRuthLab(fProj->GetTheta()));
-      fTheta->Fill(fProj->GetTheta(), xsec);
+      fKinematics->GetELab(3, th, 3, e1, e2);
+      fProj.SetEnergy(TMath::Max(e1, e2));
+      fProj.SetTheta(th);
+      fProj.SetPhi(ph);
+      xsec = TMath::Abs(fKinematics->GetXSecRuthLab(fProj.GetTheta()));
+      fTheta->Fill(fProj.GetTheta(), xsec);
       //slowing of outgoing projectile in target
       fTarget->SetOutgoing();
-      fTarget->DetectParticle(fProj);
+      fTarget->DetectParticle(&fProj);
       //now detect particle in detector(s)
-      fAlignedDetectors->R__FOR_EACH(KVDetector, DetectParticle)(fProj);
+      fAlignedDetectors->R__FOR_EACH(KVDetector, DetectParticle)(&fProj);
       //fill histograms
       fDepth->Fill(IP.z());
       int j = 0;
@@ -380,22 +365,22 @@ void KVElasticScatter::CalculateScattering(Int_t N)
          //prepare for next round: set energy loss to zero
          d->Clear();
       }
-      fProj->SetEnergy(fEnergy);
-      fProj->SetTheta(0);
+      fProj.SetEnergy(fEnergy);
+      fProj.SetTheta(0);
+      fProj.GetParameters()->Clear();
       //set random interaction point for scattering
       if (fIntLayer) {
          fTarget->SetInteractionLayer(fIntLayer, fBeamDirection);
       }
       else {
-         fTarget->GetInteractionPoint(fProj);
+         fTarget->GetInteractionPoint(&fProj);
          //if target is multilayer and the interaction layer is not fixed,
          //the layer & hence the target nucleus may change
          if (fMultiLayer) {
             targ_mat = fTarget->GetLayer(fTarget->GetInteractionPoint());
-            fTarg->SetZ((Int_t) targ_mat->GetZ());
-            fTarg->SetA((Int_t) targ_mat->GetMass());
-            fKinematics->SetTarget(fTarg);
-            fKinematics->SetOutgoing(fProj);
+            KVNucleus t;
+            t.SetZandA((Int_t) targ_mat->GetZ(), (Int_t) targ_mat->GetMass());
+            fKinematics->SetTarget(t);
          }
       }
       IP = fTarget->GetInteractionPoint();

KVMultiDet/simulation/KVElasticScatter.h
@@ -12,6 +12,7 @@ $Date: 2007/04/04 10:39:17 $
 
 #include "TVector3.h"
 #include "KVNameValueList.h"
+#include "KVNucleus.h"
 
 class TH1F;
 class TList;
@@ -20,7 +21,6 @@ class KVTelescope;
 class KVDetector;
 class KVTarget;
 class KV2Body;
-class KVNucleus;
 class TObjArray;
 
 class KVElasticScatter {
@@ -29,8 +29,7 @@ class KVElasticScatter {
    Int_t  fBinE;                //Number of bins of the Energy histogram
 
    Double_t fEnergy;            //energy of projectile
-   KVNucleus* fProj;            //scattered nucleus
-   KVNucleus* fTarg;            //target nucleus
+   KVNucleus fProj;            //scattered nucleus
 
    KV2Body* fKinematics;        //kinematics calculation
 

commit 04450df6a088cfec312b9b049cadac017d3b4f8c

fix regression in KV2Body introduced by 870cab4325550019ce99e5b9e471a975f155177a
kinematics for elastic scattering defined by putting the same nucleus as
the projectile for outgoing nucleus was not correct

KVMultiDet/db/KV2Body.cpp
@@ -170,14 +170,14 @@ void KV2Body::init()
       K[i] = 0.;
       TETAMAX[i] = 0.;
       TETAMIN[i] = 0.;
-      fThetaLabVsThetaCM[i] = 0;
-      fELabVsThetaCM[i] = 0;
-      fELabVsThetaLab[i] = 0;
+      fThetaLabVsThetaCM[i] = nullptr;
+      fELabVsThetaCM[i] = nullptr;
+      fELabVsThetaLab[i] = nullptr;
    }
-   fKoxReactionXSec = 0;
-   fEqbmChargeState = 0;
-   fEqbmChargeStateShSol = 0;
-   fEqbmChargeStateShGas = 0;
+   fKoxReactionXSec = nullptr;
+   fEqbmChargeState = nullptr;
+   fEqbmChargeStateShSol = nullptr;
+   fEqbmChargeStateShGas = nullptr;
    fSetOutgoing = kFALSE;
 
    SetIntegralPrecision(1e-10);
@@ -238,17 +238,25 @@ KV2Body::KV2Body(KVNucleus*, KVNucleus* cib, KVNucleus* proj_out, Double_t): fNu
 }
 
 KV2Body::KV2Body(const KVNucleus& compound, double Exx)
-   : fNuclei(5), fEDiss(0)
+   : fNuclei(5), fEDiss(-Exx)
 {
-   // Set up kinematics of binary decay of compound.
+   // Set up kinematics of binary decay of compound nucleus.
    //
-   // By default (Exx=0) we use the excitation energy of compound as `-Ediss`
+   // The excitation energy of the CN (in MeV) is either given:
+   //
+   //   - in the KVNucleus object itself (i.e. compound.GetExcitEnergy() gives the E* of CN)
+   //   - or in the variable Exx: in this case any E* in the KVNucleus will be ignored
+   //   - or by calling SetExcitEnergy or SetDissipatedEnergy after this constructor with the NEGATIVE E*
    //
    //Usage:
    //~~~~~~~~~~{.cpp}
    //   KVNucleus CN("204Pb");
    //   CN.SetExcitEnergy(1.5*CN.GetA());
    //   KV2Body CNdecay(CN);
+   //
+   //   /* or: KV2Body CNdecay("204Pb", 1.5*204);
+   //    * or: KV2Body CNdecay("204Pb");
+   //    *     CNdecay.SetExcitEnergy(-1.5*204); */
    //
    //   // calculate decay
    //   KVNucleus alpha("4He", 67.351/4.);
@@ -257,15 +265,15 @@ KV2Body::KV2Body(const KVNucleus& compound, double Exx)
 
    init();
    fNuclei[1] = compound;
-   if (Exx == 0.0) fEDiss = -compound.GetExcitEnergy();
+   if (fEDiss == 0) fEDiss = compound.GetExcitEnergy();
 }
 
 KV2Body::KV2Body(const KVNucleus& proj, const KVNucleus& targ, double Ediss):
-   fNuclei(5), fEDiss(0)
+   fNuclei(5), fEDiss(Ediss)
 {
    //Set up calculation of basic binary reaction for given projectile and target.
    //
-   //By default the dissipated energy is zero (elastic reaction).
+   //By default the dissipated energy Ediss is zero (elastic reaction).
    //
    //Usage:
    //~~~~~~~~~~{.cpp}
@@ -278,16 +286,18 @@ KV2Body::KV2Body(const KVNucleus& proj, const KVNucleus& targ, double Ediss):
    init();
    fNuclei[1] = proj;
    fNuclei[2] = targ;
-   fEDiss = Ediss;
 }
 
-KV2Body::KV2Body(const KVNucleus& proj, const KVNucleus& targ, const KVNucleus& proj_out, double Ediss):
+KV2Body::KV2Body(const KVNucleus& proj, const KVNucleus& targ, const KVNucleus& outgoing, double Ediss):
    fNuclei(5), fEDiss(Ediss)
 {
    //Set up calculation of basic binary reaction for given projectile and target with definition
-   //of exit channel (outgoing projectile fragment).
+   //of exit channel (outgoing projectile- or target-like fragment).
    //
    //By default the dissipated energy is zero (elastic reaction).
+   //
+   //Any excitation energy of the outgoing fragment will be taken into account, e.g.
+   //for quasi-elastic scattering leaving the target in an excited state.
    //
    //Usage:
    //~~~~~~~~~~{.cpp}
@@ -300,13 +310,15 @@ KV2Body::KV2Body(const KVNucleus& proj, const KVNucleus& targ, const KVNucleus& 
    init();
    fNuclei[1] = proj;
    fNuclei[2] = targ;
-   SetOutgoing(proj_out);
+   if (outgoing.GetExcitEnergy() > 0) fEDiss += outgoing.GetExcitEnergy();
+   SetOutgoing(outgoing);
 }
 
 void KV2Body::SetTarget(const KVNucleus& targ)
 {
    //Set target for reaction.
    fNuclei[2] = targ;
+   fSetOutgoing = kFALSE;
 }
 
 void KV2Body::SetTarget(Int_t z, Int_t a)
@@ -314,18 +326,21 @@ void KV2Body::SetTarget(Int_t z, Int_t a)
    //Set target for reaction
 
    fNuclei[2].SetZandA(z, a);
+   fSetOutgoing = kFALSE;
 }
 
 void KV2Body::SetProjectile(const KVNucleus& proj)
 {
    //Set projectile for reaction.
    fNuclei[1] = proj;
+   fSetOutgoing = kFALSE;
 }
 
 void KV2Body::SetProjectile(Int_t z, Int_t a)
 {
    //Set projectile for reaction
    fNuclei[1].SetZandA(z, a);
+   fSetOutgoing = kFALSE;
 }
 
 KV2Body::~KV2Body()
@@ -361,7 +376,8 @@ void KV2Body::SetOutgoing(const KVNucleus& proj_out)
    fSetOutgoing = kTRUE;
    fNuclei[3] = proj_out;
    Set4thNucleus();
-   GetNucleus(3)->SetExcitEnergy(0.);// no mass modification due to E*
+   // all nuclei should now have E* set to zero in order to use ground state masses in kinematics
+   for (unsigned i = 1; i <= 4; ++i) if (fNuclei[i].IsDefined()) fNuclei[i].SetExcitEnergy(0);
 }
 
 //_____________________________________________________________________________
@@ -371,19 +387,21 @@ void KV2Body::Set4thNucleus()
    // Private method, used to deduce 4th nucleus (target-like) from projectile, target
    // and outgoing projectile using conservation of mass, momentum and energy.
    //
-   // if the exit channel is a the compund nucleus, there is no 4th nucleus defined
-   // and the excitation energy of the fusion reaction is set in the fEdiss variable
-   // see GetExcitEnergy() method
+   // if the outgoing nucleus set by the user is equal to the compound nucleus
+   // formed by projectile and target, but the excitation energy of the CN
+   // was not set by the user, we calculate it here.
 
    KVNucleus sum;
-   if (GetNucleus(2)) sum = *GetNucleus(1) + *GetNucleus(2);
+   if (GetNucleus(2))
+      sum = *GetNucleus(1) + *GetNucleus(2);
    else sum = *GetNucleus(1);
    KVNucleus tmp4 = sum - *GetNucleus(3);
    if (!tmp4.IsDefined()) {
-      SetExcitEnergy(sum.GetExcitEnergy());
+      // nucleus 3 is the CN proj+targ
+      // has E* been defined ?
+      if (fEDiss == 0) fEDiss = sum.GetExcitEnergy();
    }
    fNuclei[4] = tmp4;
-   if (tmp4.IsDefined()) tmp4.SetExcitEnergy(0.); // no mass modification due to E*
 }
 
 //_____________________________________________________________________________
@@ -579,14 +597,9 @@ void KV2Body::CalculateKinematics()
       Error("CalculateKinematics", "Set outgoing (decay) product first");
       return;
    }
-   // make sure E* of nuclei is zero
-   // this is because the E*/Ediss is handled separately here
-   Nuc1->SetExcitEnergy(0.);
-   if (Nuc2) Nuc2->SetExcitEnergy(0.);
 
    // call SetOutgoing if not already done
    if (!fSetOutgoing) SetOutgoing(*Nuc1);
-   //fSetOutgoing = kFALSE;
 
    // set everything to zero
    WLT = WCT = BCM = 0.;

KVMultiDet/db/KV2Body.h
@@ -76,7 +76,7 @@ public:
    KV2Body(const Char_t* systemname);
    KV2Body(const KVNucleus& compound, double Exx = 0.0);
    KV2Body(const KVNucleus& proj, const KVNucleus& targ, double Ediss = 0.0);
-   KV2Body(const KVNucleus& proj, const KVNucleus& targ, const KVNucleus& proj_out, double Ediss = 0.0);
+   KV2Body(const KVNucleus& proj, const KVNucleus& targ, const KVNucleus& outgoing, double Ediss = 0.0);
    KV2Body(KVNucleus*, KVNucleus* = nullptr, KVNucleus* = nullptr, Double_t = 0.0);
    virtual ~ KV2Body();
 
@@ -90,18 +90,27 @@ public:
    void SetTarget(Int_t z, Int_t a = 0);
    void SetOutgoing(const KVNucleus& proj_out);
 
+   /// Set excitation energy of exit channel [MeV]
+   /// i.e. this corresponds to the energy dissipated in the reaction
+   ///
+   /// To set E* of compound nucleus in entrance channel for binary break-up
+   /// calculations, give negative value (or see relevant constructor)
    void SetExcitEnergy(Double_t ex)
    {
       fEDiss = ex;
-   };
+   }
+   /// Returns the excitation energy of the exit channel [MeV]
+   /// i.e. this corresponds to the energy dissipated in the reaction
    Double_t GetExcitEnergy() const
    {
       return fEDiss;
    }
+   /// Set energy dissipated in the reaction [MeV]
    void SetEDiss(Double_t ex)
    {
       SetExcitEnergy(ex);
    }
+   /// Returns energy dissipated in the reaction [MeV]
    Double_t GetEDiss() const
    {
       return GetExcitEnergy();
@@ -131,7 +140,7 @@ public:
          gamma = 0.0;
       }
       return gamma;
-   };
+   }
 
    TF1* GetThetaLabVsThetaCMFunc(Int_t OfNucleus) const;
    TF1* GetELabVsThetaCMFunc(Int_t OfNucleus) const;
@@ -141,19 +150,19 @@ public:
    {
       // Calculate lab angle of nucleus OfNucleus (=1,2,3,4) as a function of CM angle
       return const_cast<KV2Body*>(this)->GetThetaLabVsThetaCMFunc(OfNucleus)->Eval(ThetaCM);
-   };
+   }
    Double_t GetELab(Double_t ThetaCM, Int_t OfNucleus) const
    {
       // Calculate lab energy of nucleus OfNucleus (=1,2,3,4) as a function of CM angle
       return const_cast<KV2Body*>(this)->GetELabVsThetaCMFunc(OfNucleus)->Eval(ThetaCM);
-   };
+   }
    Int_t GetThetaCM(Double_t ThetaLab, Int_t OfNucleus, Double_t& t1, Double_t& t2) const;
    Double_t GetThetaCM(Int_t OfNucleus, Double_t theta, Int_t OtherNucleus) const
    {
       // Calculate projectile CM angle from target CM angle and vice versa
       if (TMath::Abs(OfNucleus - OtherNucleus) % 2) return 180. - theta;
       return theta;
-   };
+   }
    Double_t GetMinThetaCMFromThetaLab(Int_t OfNucleus, Double_t theta, Int_t OtherNucleus) const;
 
    Int_t GetELab(Int_t OfNucleus, Double_t ThetaLab, Int_t AngleNucleus, Double_t& e1, Double_t& e2) const;