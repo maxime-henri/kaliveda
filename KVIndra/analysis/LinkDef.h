a302fbdf9c70d4f5eca7cf3e0bdb2ef9b7214cfb
John Frankland <john.frankland@ganil.fr>
2020-04-06 18:33:34 +0200

    remove obsolete INDRA analysis/reconstruction classes
     * also remove INDRAVAMOS option - does not compile

--- KVIndra/analysis/LinkDef.h
+++ KVIndra/analysis/LinkDef.h
@@ -4,23 +4,14 @@
 #pragma link off all classes;
 #pragma link off all functions;
 #ifdef WITH_BUILTIN_GRU
-#pragma link C++ class KVINDRARawDataAnalyser;
-#pragma link C++ class KVINDRARawDataReconstructor;
-#endif
 #pragma link C++ class KVINDRADstToRootTransfert;
+#endif
 #pragma link C++ class KVINDRAReconDataAnalyser;
 #pragma link C++ class KVINDRAGeneDataSelector;
 #pragma link C++ class KVINDRAGeneDataAnalyser;
 #pragma link C++ class KVOldINDRASelector+;
 #pragma link C++ class KVINDRAEventSelector+;
 #pragma link C++ class KVDataSelector;
-#pragma link C++ class KVINDRAReconIdent+;
-#pragma link C++ class KVReconIdent_e475s+;
-#pragma link C++ class KVINDRAIdentRoot+;
-#pragma link C++ class KVINDRAReconRoot+;
-#ifdef WITH_GRULIB
-#pragma link C++ class KVINDRAOnlineDataAnalyser+;
-#endif
 #pragma link C++ class KVDataPatch_INDRA_SanityChecks+;
 #pragma link C++ class KVDataPatch_INDRA_camp5_PHDcorrection+;
 #pragma link C++ class KVDataPatch_CorrectEtalonModuleIDCode+;
