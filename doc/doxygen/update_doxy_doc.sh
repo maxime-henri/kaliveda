942c33f4b76c02112ed09c67852bc62bf5f2147b
John Frankland <john.frankland@ganil.fr>
2020-07-07 09:16:29 +0200

    doc: use mathjax to render formulae and include links to ROOT class doc

--- doc/doxygen/update_doxy_doc.sh
+++ doc/doxygen/update_doxy_doc.sh
@@ -7,4 +7,11 @@ kaliveda -b -q
 
 export KALIVEDA_VERSION=$(kaliveda-config --version)
 cp kaliveda.git/README.md kaliveda.doxygen/
+# kludge
+cp kaliveda.git/KVMultiDet/impact_parameter/ipde_fit_parameter.h kaliveda.doxygen/KVMultiDet/impact_parameter/
+# download ROOT tag file to link ROOT documentation
+if [ ! -f ./ROOT.tag ]; then
+   wget https://root.cern/doc/master/ROOT.tag
+fi
+
 export INPUT_DIR=kaliveda.doxygen && doxygen $DOXYFILE
