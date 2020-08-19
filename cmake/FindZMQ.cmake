13f721092c04290be7b1a9402ecb9eddab5dad59
John Frankland <john.frankland@ganil.fr>
2020-08-19 14:15:52 +0200

    use pkg-config to locate protobuf and ZeroMQ if necessary
     * make sure env var PKG_CONFIG_PATH contains all necessary paths to find
       '.pc' files

--- cmake/FindZMQ.cmake
+++ cmake/FindZMQ.cmake
@@ -4,8 +4,37 @@
 # ZMQ_INCLUDE_DIRS - The ZMQ include directories
 # ZMQ_LIBRARIES - The libraries needed to use ZMQ
 
-find_path ( ZMQ_INCLUDE_DIR zmq.hpp )
-find_library ( ZMQ_LIBRARY NAMES zmq )
+find_path (ZMQ_INCLUDE_DIR
+      NAMES zmq.hpp
+      )
+      
+find_library (ZMQ_LIBRARY
+      NAMES zmq
+      )
+      
+if((NOT ZMQ_INCLUDE_DIR) OR (NOT ZMQ_LIBRARY))
+   ## load in pkg-config support
+   find_package(PkgConfig QUIET)
+   
+   if(PkgConfig_FOUND)
+      ## use pkg-config to get hints for 0mq locations
+      pkg_check_modules(PC_ZeroMQ QUIET libzmq)
+
+      ## use the hint from above to find where 'zmq.hpp' is located
+      find_path(ZMQ_INCLUDE_DIR
+        NAMES zmq.hpp
+        PATHS ${PC_ZeroMQ_INCLUDE_DIRS}
+        )
+
+      ## use the hint from about to find the location of libzmq
+      find_library(ZMQ_LIBRARY
+        NAMES zmq
+        PATHS ${PC_ZeroMQ_LIBRARY_DIRS}
+        )
+   endif(PkgConfig_FOUND)
+   
+endif((NOT ZMQ_INCLUDE_DIR) OR (NOT ZMQ_LIBRARY))
+
 set ( ZMQ_LIBRARIES ${ZMQ_LIBRARY} )
 set ( ZMQ_INCLUDE_DIRS ${ZMQ_INCLUDE_DIR} )
 
